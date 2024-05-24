37af74c37a1ef9421fcbe13906dd5cde1f55025b
*史* *昆* *靈* <k955339962@gmail.com>
2024-05-24 16:01:53 +0800

    #0524 F

--- prjMSIT158site/Program.cs
+++ prjMSIT158site/Program.cs
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Homework}/{action=Homework5}/{id?}");
 
 app.Run();

48b29daba57b05b7ea920a3d98889854f872893b
*史* *昆* *靈* <k955339962@gmail.com>
2024-05-24 16:57:10 +0800

    #0524 FF

---
+++ prjMSIT158API/Models/DTO/CSpotsPagingDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace prjMSIT158API.Models.DTO
+{
+    public class CSpotsPagingDTO
+    {
+        public int TotalPages { get; set; }
+        public int TotalCount { get; set; }
+        public List<SpotImagesSpot>? SpotsResult { get; set; }
+    }
+}
